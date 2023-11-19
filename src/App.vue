3a743b58b89115c60631157c1bd2f0a53829cdc9
rpsobolewski <rpsobolewski@gmail.com>
2023-11-19 22:15:28 +0100

    console log data

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: "App",
+
+
+
+  data() {
+
+    return {
+
+
+
+      base_url: 'http://127.0.0.1:8000',
+      project_api: '/api/projects',
+
+      projects: [],
+
+
+    }
+
+  },
+
+
+  mounted() {
+    axios
+      .get(this.base_url + this.project_api)
+      .then(response => {
+        console.log(response);
+        this.project = response.data.result
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>api</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style lang="scss"></style>
