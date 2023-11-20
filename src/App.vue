f1e2d94f2bc94cbca0271c20c0dff7ba7314785d
rpsobolewski <rpsobolewski@gmail.com>
2023-11-20 01:35:42 +0100

    add ProjectCard component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
+<!-- App.vue -->
+<template>
+  <div>
+    <h1>Projects</h1>
+    <ul>
+      <li v-for="project in projects.data" :key="project.id">
+        <ProjectCard :project="project" />
+      </li>
+    </ul>
+  </div>
+</template>
+
 <script>
 import axios from 'axios';
-
+import ProjectCard from './components/ProjectCard.vue'; // Adjust the path based on your project structure
 
 export default {
-  props: {
-
+  components: {
+    ProjectCard,
   },
-  name: 'App',
-
   data() {
     return {
       base_url: 'http://127.0.0.1:8000',
@@ -15,8 +25,6 @@ export default {
       projects: [],
     };
   },
-
-
   mounted() {
     axios
       .get(this.base_url + this.project_api)
@@ -28,31 +36,9 @@ export default {
         console.error(err);
       });
   },
-
 };
 </script>
-<template>
-  <div>
-    <h1>Projects</h1>
-    <ul>
-      <li v-for="project in projects.data" :key="project.id">
-        <h2>{{ project.title }}</h2>
-        <p>{{ project.description }}</p>
-        <img :src="project.thumb" alt="Project Thumbnail" style="max-width: 100%; height: auto;">
-        <!-- Add more project information as needed -->
-        <p>Type: {{ project.type.name }}</p>
-        <p>Technologies:
-          <span v-for="technology in project.technologies" :key="technology.id">{{ technology.name }} </span>
-        </p>
-        <p>Link:
-          <a :href="project.link_project" target="_blank">Project</a>
-          <a v-if="project.link_github" :href="project.link_github" target="_blank">GitHub</a>
-        </p>
-        <p>Created At: {{ project.created_at }}</p>
-      </li>
-    </ul>
-  </div>
-</template>
 
-
-<style lang="scss"></style>
+<style>
+/* Add your component-specific styles here */
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <div>
+        <h2>{{ project.title }}</h2>
+        <p>{{ project.description }}</p>
+        <img :src="project.thumb" alt="Project Thumbnail" style="max-width: 100%; height: auto;">
+        <p>Type: {{ project.type.name }}</p>
+        <p>Technologies:
+            <span v-for="technology in project.technologies" :key="technology.id">{{ technology.name }} </span>
+        </p>
+        <p>Link:
+            <a :href="project.link_project" target="_blank">Project</a>
+            <a v-if="project.link_github" :href="project.link_github" target="_blank">GitHub</a>
+        </p>
+        <p>Created At: {{ project.created_at }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped></style>
